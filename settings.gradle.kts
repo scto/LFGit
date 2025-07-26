7822b79af3745448a2eb08c752d82f11cd358f02
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-07-26 20:44:52 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+    maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
+  }
+}
+
+//plugins {
+//  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+//}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://jitpack.io") }
+  }
+}
+
+rootProject.name = "LFGIT
+
+include(":app")
